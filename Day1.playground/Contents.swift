661e9b7b9cb9dc00e07da227067eb23db0abe826
Judy <jyu@lcs.on.ca>
2021-11-01 21:57:58 -0400

    Clone from GitHub, create a variable and change its value two times

--- Day1.playground/Contents.swift
+++ Day1.playground/Contents.swift
@@ -2,4 +2,18 @@ import Cocoa
 
 // Day 1 - variables, constants, strings, and numbers
 
-var greeting = "Hello, playground"
+//variable
+var favouriteFood = "Ice cream"
+print(favouriteFood)
+
+favouriteFood = "Ramen"
+print(favouriteFood)
+
+favouriteFood = "Chocolate brownies"
+print(favouriteFood)
+
+//constants
+let
+
+
+//Day 0 - introduction
