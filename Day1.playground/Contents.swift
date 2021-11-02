4482ac7809422a59cf33e23ac5aed745ef1c8592
Judy <jyu@lcs.on.ca>
2021-11-01 22:14:25 -0400

    Clone codes fro GitHub, create a variable and change its value

--- Day1.playground/Contents.swift
+++ Day1.playground/Contents.swift
@@ -17,8 +17,17 @@ let theYearIWasBorn = 2005
 print(theYearIWasBorn)
 
 //strings
-let thisIsAParagraph = """
+let thisIsASentence = "Lorem ipsum dolor \"sit amet\", consectetur adipiscing elit."
+print(thisIsASentence)
+print(thisIsASentence.count)
+print(thisIsASentence.hasPrefix("Lorem ipsum"))
 
+let thisIsAParagraph = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris augue neque gravida in fermentum et.
 """
+print(thisIsAParagraph)
+
+//numbers
+
 
 //Day 0 - introduction
